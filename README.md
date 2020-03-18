ea41a5c79f6001cebe2a68ec8eb861813da9e773
Package <Aditrc@outlook.com>
2020-03-18 18:53:49 +0800

    fix bug

--- README.md
+++ README.md
@@ -2,18 +2,30 @@
 `HashMap/Set<Pin<Arc<T>>>` based fully safety tree collection
 ## Examples
 ```rust
-let pt = PinTree::<i32>::new();
+let mut pt = PinTree::<i32>::new();
 
-let a = pt.node(1);
-let b = pt.node(2);
-let c = pt.node(2);
+let a = &pt.node(1);
+let b = &pt.node(2);
+let c = &pt.node(3);
 
 pt.set_parent(b, a);
 pt.set_parent(c, a);
 //    a
-//  ↙ ↘
-// b    c
+//  ↙  ↘
+// b     c
 
 assert_eq!(pt.is_parent(b, a), true);
-assert_eq!(pt.is_child(c, a), true);
+assert_eq!(pt.is_child(a, c), true);
+```
+```rust
+// Circular references are safe
+pt.set_parent(b, a);
+pt.set_parent(a, c);
+pt.set_parent(c, b);
+//    a
+//  ↙  ↖
+// b  →  c
+
+pt.set_parent(a, a);
+// a ⟲
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,40 @@
 //! HashMap/Set<Pin<Arc<T>>>` based fully safety tree collection
 //! # Examples
 //! ```
-//! let pt = PinTree::<i32>::new();
-//! 
-//! let a = pt.node(1);
-//! let b = pt.node(2);
-//! let c = pt.node(2);
-//! 
+//! let mut pt = PinTree::<i32>::new();
+//!
+//! let a = &pt.node(1);
+//! let b = &pt.node(2);
+//! let c = &pt.node(3);
+//!
 //! pt.set_parent(b, a);
 //! pt.set_parent(c, a);
 //! //    a
-//! //  ↙ ↘
-//! // b    c
-//! 
+//! //  ↙  ↘
+//! // b     c
+//!
 //! assert_eq!(pt.is_parent(b, a), true);
-//! assert_eq!(pt.is_child(c, a), true);
+//! assert_eq!(pt.is_child(a, c), true);
+//! ```
+//! ```
+//! // Circular references are safe
+//! pt.set_parent(b, a);
+//! pt.set_parent(a, c);
+//! pt.set_parent(c, b);
+//! //    a
+//! //  ↙  ↖
+//! // b  →  c
+//!
+//! pt.set_parent(a, a);
+//! // a ⟲
 //! ```
 //! See [PinTree](struct.PinTree.html)
 
 use std::collections::hash_set::Iter;
 use std::collections::{HashMap, HashSet};
 use std::convert::AsRef;
+use std::fmt;
+use std::fmt::{Display, Formatter};
 use std::hash::{Hash, Hasher};
 use std::ops::Deref;
 use std::pin::Pin;
@@ -39,6 +53,7 @@ where
 }
 
 /// `HashMap/Set<Pin<Arc<T>>>` based fully safety tree collection
+#[derive(Debug, Clone)]
 pub struct PinTree<T> {
     nodes: HashSet<PinNode<T>>,
     parents: HashMap<PinNode<T>, PinNode<T>>,
@@ -56,14 +71,18 @@ impl<T> PinTree<T> {
         }
     }
     /// Create a PinNode and add it to PinTree
-    pub fn node(&mut self, v: T) -> &PinNode<T> {
+    pub fn node(&mut self, v: T) -> PinNode<T> {
         let n: PinNode<T> = PinNode::new(v);
         self.nodes.insert(n.clone());
-        self.nodes.get(&n).unwrap()
+        n
     }
     /// add a PinNode to PinTree
     pub fn node_from(&mut self, node: PinNode<T>) -> bool {
         self.nodes.insert(node)
+    }
+    /// Check if PinTree has node
+    pub fn has(&self, node: &PinNode<T>) -> bool {
+        self.nodes.contains(node)
     }
     fn remove_child(&mut self, parent: &PinNode<T>, this: &PinNode<T>) -> bool {
         if let Some(childs) = self.childs.get_mut(parent) {
@@ -91,7 +110,7 @@ impl<T> PinTree<T> {
     }
     /// Unset parent-child relationship   
     pub fn unset_parent(&mut self, this: &PinNode<T>, parent: &PinNode<T>) -> bool {
-        if self.is_parent(this, parent) {
+        if !self.is_parent(this, parent) {
             return false;
         }
         self.parents.remove(&this);
@@ -106,10 +125,10 @@ impl<T> PinTree<T> {
             .unwrap_or(false)
     }
     /// Check if it's child
-    pub fn is_child(&self, this: &PinNode<T>, parent: &PinNode<T>) -> bool {
+    pub fn is_child(&self, this: &PinNode<T>, child: &PinNode<T>) -> bool {
         self.childs
-            .get(parent)
-            .map(|childs| childs.contains(this))
+            .get(this)
+            .map(|childs| childs.contains(child))
             .unwrap_or(false)
     }
     /// Get the parent of node
@@ -117,9 +136,9 @@ impl<T> PinTree<T> {
         self.parents.get(&this)
     }
     /// Get the childs of node
-    pub fn get_childs(&self, parent: &PinNode<T>) -> Iter<PinNode<T>> {
+    pub fn get_childs(&self, this: &PinNode<T>) -> Iter<PinNode<T>> {
         self.childs
-            .get(parent)
+            .get(this)
             .map(|childs| childs.iter())
             .unwrap_or(self._empty_node_set.iter())
     }
@@ -132,13 +151,24 @@ impl<T> PinTree<T> {
             self.remove_child(&parent, this);
             self.parents.remove(this);
         }
+        if self.childs.contains_key(this) {
+            for child in self.childs.remove(this).unwrap().iter() {
+                self.parents.remove(child);
+            }
+        }
         self.nodes.remove(this);
         true
+    }
+}
+impl<T> Display for PinTree<T> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "PinTree{{...}}")
     }
 }
 
 /// PinNode is a PinArc Box  
 /// Wrap of Pin<Arc<T>>
+#[derive(Debug)]
 pub struct PinNode<T> {
     inner: Pin<Arc<T>>,
 }
@@ -179,5 +209,96 @@ impl<T> Deref for PinNode<T> {
 impl<T> AsRef<T> for PinNode<T> {
     fn as_ref(&self) -> &T {
         self.deref()
+    }
+}
+impl<T> Display for PinNode<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "PinNode({})", self.inner)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_base() {
+        let mut pt = PinTree::<i32>::new();
+
+        let a = &pt.node(1);
+        let b = &pt.node(2);
+        let c = &pt.node(3);
+
+        pt.set_parent(b, a);
+        pt.set_parent(c, a);
+
+        assert_eq!(pt.is_parent(b, a), true);
+        assert_eq!(pt.is_child(a, c), true);
+    }
+
+    #[test]
+    fn test_circular_ref() {
+        let mut pt = PinTree::<i32>::new();
+
+        let a = &pt.node(1);
+        let b = &pt.node(2);
+        let c = &pt.node(3);
+
+        pt.set_parent(b, a);
+        pt.set_parent(a, c);
+        pt.set_parent(c, b);
+
+        assert_eq!(pt.is_parent(b, a), true);
+        assert_eq!(pt.is_parent(a, c), true);
+        assert_eq!(pt.is_parent(c, b), true);
+
+        let d = &pt.node(0);
+        pt.set_parent(d, d);
+        assert_eq!(pt.is_parent(d, d), true);
+    }
+
+    #[test]
+    fn test_unset() {
+        let mut pt = PinTree::<i32>::new();
+
+        let a = &pt.node(1);
+        let b = &pt.node(2);
+
+        assert_eq!(pt.is_parent(b, a), false);
+
+        pt.set_parent(b, a);
+        assert_eq!(pt.is_parent(b, a), true);
+
+        pt.unset_parent(b, a);
+        assert_eq!(pt.is_parent(b, a), false);
+    }
+
+    #[test]
+    fn test_get_val() {
+        let mut pt = PinTree::<i32>::new();
+
+        let a = &pt.node(1);
+
+        println!("{}", a);
+        assert_eq!(**a, 1);
+    }
+
+    #[test]
+    fn test_remove() {
+        let mut pt = PinTree::<i32>::new();
+
+        let a = &pt.node(1);
+        let b = &pt.node(2);
+
+        assert_eq!(pt.is_parent(b, a), false);
+
+        pt.set_parent(b, a);
+        assert_eq!(pt.is_parent(b, a), true);
+
+        pt.remove(a);
+        assert_eq!(pt.is_parent(b, a), false);
     }
 }
